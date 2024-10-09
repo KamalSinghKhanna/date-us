3e4d8e926956d7862d1de23f71c72ecfdbc8edba
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2024-10-09 11:33:03 +0530

    middleware code

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
 const express = require("express");
+const { authAdmin, authUser } = require("../middleware/auth");
 
 const app = express();
+// app.use("/admin", authAdmin);
+app.get("/admin/getAllData", authAdmin, (req, res) => {
+  //logic if the req is authorized or validate token
 
-app.use("/user", [
-  (req, res, next) => {
-    console.log("result");
-
-    // res.send("router handler 1")
-    next();
-  },
-  (req, res, next) => {
-    console.log("result2");
-    // res.send("router handler 2");
-    next();
-  },
-  (req, res, next) => {
-    console.log("result3");
-    res.send("router handler 3");
-    next();
-  },
-]);
+  res.send("all data sent");
+});
+app.delete("/admin/deleteUser", (req, res) => {
+  res.end("delete user");
+});
+app.get("/user", authUser ,(req, res) => {
+  res.end("delete user");
+});
 
 app.listen(3000, () => {
   console.log("server listining");
